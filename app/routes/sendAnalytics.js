d32c8e5b2081a385e3d7d82a27fc98f7a0ead629
indexkay <kavinlajara@gmail.com>
2025-03-18 00:00:31 -0400

    -- prisma update functionallty

--- app/routes/sendAnalytics.js
+++ app/routes/sendAnalytics.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
 
 // Set up Express server
 const app = express();
@@ -31,7 +34,7 @@ const webData = {
 };
 
 // Route to receive analytics data
-app.post('/sendAnalytics', (req, res) => {
+app.post('/sendAnalytics', async(req, res) => {
   try {
     const data = req.body; // Data sent from client
     console.log('Received Analytics Data:', data);
@@ -40,6 +43,35 @@ app.post('/sendAnalytics', (req, res) => {
     webData.totalViews += data.totalViews;
     webData.newVisitors += data.newVisitors;
     webData.sessionDuration += data.sessionDuration;
+
+    const analyticsUpdate = await prisma.clients.update({
+      where: {
+        id: `67d87870bb629dbbf656a649`,
+      },
+      data: {
+        analytics: {
+          device_type: 
+          { 
+            android: 0, 
+            desktop: 0, 
+            ios: 0, 
+            mobile: 0, 
+            tablet: 0 
+          },
+          new_visitors: data.newVisitors,
+          referal_source: 
+          { 
+            direct: 0, 
+            facebook: 0, 
+            google: 0, 
+            instagram: 0, 
+            youtube: 0 
+          },
+          session_duration: data.sessionDuration,
+          total_visitors: data.totalViews
+        }
+      },
+    })
 
     // Respond with success
     res.status(200).json({ success: true, message: 'Data received and stored.' });
